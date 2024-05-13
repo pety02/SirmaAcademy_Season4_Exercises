ebefc120cb8262971f7f03971ef6b93922faf0bc
pety02 <pety02@abv.bg>
2024-05-14 00:24:52 +0300

    Speed calculator task finished!

---
+++ src/task11/SpeedCalculator.java
@@ -0,0 +1,19 @@
+package task11;
+
+import java.util.Scanner;
+
+public class SpeedCalculator {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int distanceInMeters = scanner.nextInt();
+        int hours = scanner.nextInt();
+        int minutes = scanner.nextInt();
+        int seconds = scanner.nextInt();
+
+        int realMinutes = (hours + (minutes / 60)) * 60 + (minutes % 60);
+        double realTimeInSeconds = 0.0 + (realMinutes * 60 + seconds);
+        double velocity = distanceInMeters / realTimeInSeconds;
+        System.out.println(velocity);
+    }
+}
