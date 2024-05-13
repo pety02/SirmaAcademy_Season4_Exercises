c11af663d092998cbe6d86c8f512185e6856248e
pety02 <pety02@abv.bg>
2024-05-14 00:24:35 +0300

    Time converter task finished!

---
+++ src/task10/TimeConverter.java
@@ -0,0 +1,19 @@
+package task10;
+
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+public class TimeConverter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int input = scanner.nextInt();
+        int hours = input / 60;
+        int minutes = input % 60;
+        LocalTime time = LocalTime.of(hours, minutes);
+
+        // The task's description is incorrect. There is no HR:MM time format.
+        // The correct time format is HH:mm
+        System.out.println(time.format(DateTimeFormatter.ofPattern("HH:mm")));
+    }
+}
