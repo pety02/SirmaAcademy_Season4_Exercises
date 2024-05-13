7601000930401d1a7f70f6bc6c1171a12c9e1d02
pety02 <pety02@abv.bg>
2024-05-14 00:23:48 +0300

    Distance converter task finished!

---
+++ src/task8/DistanceConverter.java
@@ -0,0 +1,12 @@
+package task8;
+
+import java.util.Scanner;
+
+public class DistanceConverter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double kilometers = scanner.nextDouble();
+        double miles = kilometers * 0.621371192;
+        System.out.println(miles);
+    }
+}
